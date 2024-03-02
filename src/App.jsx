72bbc262ca9c5e6318782a9fe753bff1fb3c8cc6
godfather101 <godfather101@godfather101.kali>
2024-03-02 14:10:13 +0530

    add files

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,6 @@ export default function App() {
                 <Route path='/' element={<Page1 />} />
                 <Route path='/page2' element={<Page2 />} />
                 <Route path='/page3' element={<Page3 />} />
-                <Route path='*' element={<Notfound />} />
             </Routes>
         </BrowserRouter>
     );
